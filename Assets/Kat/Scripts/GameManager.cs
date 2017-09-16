55bbd22cb6a09abc47131f519d83ba052cc8beee
Katherine Harris <kvm.harris@gmail.com>
2017-09-16 12:36:55 -0700

    Oculus SDK added and removed Oculus Avatar SDK

--- Assets/Kat/Scripts/GameManager.cs
+++ Assets/Kat/Scripts/GameManager.cs
@@ -11,12 +11,17 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        _level = 0; 
         DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            _level += 1;
+            ChangeScene(_level);
+        }
 	}
 
     // Play animation i
