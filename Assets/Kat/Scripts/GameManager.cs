5b9539b622dc1cac0b64eaaff1d9a8439a50815e
Katherine Harris <kvm.harris@gmail.com>
2017-09-16 01:20:42 -0700

    Add GameManager

---
+++ Assets/Kat/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private int _level;
+    private int _score;
+
+    public GameObject MenuSystem; 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
